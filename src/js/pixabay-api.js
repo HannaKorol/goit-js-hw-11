e7ee50b4a211bc8c3794c17c87a8be111d508c4b
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-08-06 09:29:40 +0200

    added Search form, HTTP request(not compl),

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,25 @@
+
+export function searchImagesByQuery(query) {
+    const URL = "https://pixabay.com/api/";
+    const API_KEY = "45296804-0fb55f0e1381bd4cbf585a7a5";
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: query,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,  
+    });
+
+    return fetch(`${URL}?${params.toString()}`)
+    .then((response)=> {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+
+.catch(error => {
+    console.error("Image Search Error:", error);
+    throw error;
+});
+}
